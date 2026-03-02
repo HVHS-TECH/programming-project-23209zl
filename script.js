fb96852f0b7703f0803ba1adf4afe97aaee9c08a
Jackson Li <23209zl@hvhs.school.nz>
2026-03-03 12:29:24 +1300

    Spell 3 Tuesday

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
+/*******************************************************/
+// Game Project
+/// Written by Jackson
+/*******************************************************/
+
+/*******************************************************/
+// setup()
+/*******************************************************/
+
+//Variables
+const height = 900;
+const width = 900;
+
+
+function preload() {
+imgBG = "/background.jpg"
+}
+
 function setup() {
-cnv = new Canvas(900, 900);
+    cnv = new Canvas(height, width);
 
-wallLH = new Sprite(0, height/2, 8, height, 'k');
-wallLH.color = 'black';
+    wallLH = new Sprite(0, height / 2, 8, height, 'k');
+    wallLH.color = 'black';
 
-wallRH = new Sprite(width, height/2, 8, height, 'k');
-wallRH.color = 'black';
+    wallRH = new Sprite(width, height / 2, 8, height, 'k');
+    wallRH.color = 'black';
 
-wallTop = new Sprite(width/2, 0, width, 8, 'k');
-wallTop.color = 'black';
+    wallTop = new Sprite(width / 2, 0, width, 8, 'k');
+    wallTop.color = 'black';
 
-wallBot = new Sprite(width/2, height, width, 8, 'k');
-wallBot.color = 'black';
+    wallBot = new Sprite(width / 2, height, width, 8, 'k');
+    wallBot.color = 'black';
 
 }
 
+function startGame(){
+
+}
+
+/*******************************************************/
+// draw()
+/*******************************************************/
 function draw() {
-background('lightblue');
+    background(imgBG);
 }
 
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
