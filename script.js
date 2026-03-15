16541c95d872b9b733677b5e321756824a48927f
Jackson Li <23209zl@hvhs.school.nz>
2026-03-16 11:00:05 +1300

    Spell 2 Monday

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 //Variables
 const height = 900;
 const width = 900;
-let time = 90;
+let time = 30;
 let gameStarted = false;
 let score = 0;
 
@@ -43,13 +43,15 @@ function setup() {
 
     BballBackboard = new Sprite(width / 2, 300, 250, 150, 'k');
     BballBackboard.image = (imgBackboard);
-    imgBackboard.resize(300, 250); 
+    imgBackboard.resize(300, 250);
 
     StartBtn = new Sprite(width / 2, 800, 150, 50, 'k');
-    StartBtn.color = '#FFFDD0';
+    StartBtn.color = '#b5fd84';
     StartBtn.text = 'Start Game';
     StartBtn.textSize = (25);
     StartBtn.visible = true;
+
+
 }
 
 /*******************************************************/
@@ -67,29 +69,34 @@ function draw() {
         gameStarted = true;
         StartBtn.visible = false;
         BballBackboard.vel.x = 3;
-        Bball.rotationSpeed = 2;
+        Bball.rotationSpeed = 3;
     }
+    //When start button is pressed game starts
 
     if (gameStarted) {
-        if (BballBackboard.collides(wallLH)) {
+        if (BballBackboard.x <= wallLH.x) {
             BballBackboard.vel.x = 3;
         }
+        //When backboard center x position gets to left wall it goes right
 
-        if (BballBackboard.collides(wallRH)) {
+        if (BballBackboard.x >= wallRH.x) {
             BballBackboard.vel.x = -3;
         }
-        
+        //When backboard center x position gets to right wall it goes left
+
         if (kb.presses('space')) {
             Bball.vel.y = -6;
         }
+        //Shoots the basketball when space is pressed
 
         if (Bball.collides(wallTop)) {
             Bball.vel.y = 0;
             Bball.vel.x = 0;
             Bball.x = width / 2;
-            Bball.rotationSpeed = 2;
+            Bball.rotationSpeed = 3;
             Bball.y = 650;
         }
+        //Resets the basketball when hits top wall
 
         if (Bball.collides(BballBackboard)) {
             score = score + 1;
@@ -99,10 +106,12 @@ function draw() {
             Bball.vel.y = 0;
             Bball.vel.x = 0;
             Bball.x = width / 2;
-            Bball.rotationSpeed = 2;
+            Bball.rotationSpeed = 3;
             Bball.y = 650;
         }
+        //Resets the basketball when hits the backboard and adds the score
     }
+
 }
 
 
